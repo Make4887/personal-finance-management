793d8b7ac3585fbd41e7cf078cc50c3c661f5576
Артем Плотников <artemplotnikov1034@gmail.com>
2023-07-12 15:20:54 +0300

    Добавлено оформление таблиц, создана директория с медиа-файлами.

--- budjet/budjet/settings.py
+++ budjet/budjet/settings.py
@@ -135,6 +135,10 @@ STATICFILES_DIRS = [
     "/static/",
 ]
 
+MEDIA_ROOT = f'{BASE_DIR}/media'
+
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
